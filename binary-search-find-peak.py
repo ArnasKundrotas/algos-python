d23f2d8adcf6fce16840050d222b128c665056f7
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-27 21:09:47 +0300

    Added find peak project UNSOLVED

---
+++ binary-search-find-peak.py
@@ -0,0 +1,11 @@
+##############
+# UNSOLVED 
+##############
+# Binary search
+# "Find peak"
+# arr increases and then decreases
+# find the maximum
+# arr       [2, 3,  4,  6,  9,  12, 11, 8,  6,  4,  1]
+# index      0  1   2   3   4   5   6   7   8   9   10   
+#           left                mid                 right
+##############
